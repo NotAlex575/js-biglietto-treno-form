4b3a68305d835105b86fa1d40b2af1613910fd4f
NotAlex575 <dapontealessandro89gmail.com>
2025-07-10 18:05:54 +0200

    Aggiunti punti finali css/bootstrap

--- script/script.js
+++ script/script.js
@@ -4,16 +4,23 @@ const genera_info = document.getElementById('genera-info');
 const elimina_info = document.getElementById('elimina-info');
 
 const displayElemento = document.getElementById("display");
+const displayElementoTesto = document.getElementById("display-testo");
 const info = document.getElementById('test');
 const eta = document.getElementById('eta');
 const costo_totale = document.getElementById('costo-totale')
 
 
+
 genera_info.addEventListener('click', function(){
     const numero_km = km_da_percorrere.value;
-    const dati_nome = nome_cognome.value;
+    let dati_nome = nome_cognome.value;
+    if(dati_nome == ""){
+        dati_nome = "Sconosciuto"
+    }
     const eta_valore = eta.value;
     info.innerHTML = dati_nome;
+    displayElementoTesto.classList.remove("display-nascosto");
+    displayElementoTesto.classList.add("display-visibile-testo");
     displayElemento.classList.remove("display-nascosto");
     displayElemento.classList.add("display-visibile");
 
@@ -28,5 +35,12 @@ genera_info.addEventListener('click', function(){
         prezzo = prezzo.toFixed(2);
     }
     costo_totale.innerHTML = prezzo
+})
+
+elimina_info.addEventListener('click', function(){
+    displayElementoTesto.classList.remove("display-visibile-testo");
+    displayElementoTesto.classList.add("display-nacosto");
+    displayElemento.classList.add("display-nascosto");
+    displayElemento.classList.remove("display-visibile");
 })
 
